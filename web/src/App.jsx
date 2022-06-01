64a492915e442547e02e6b5f0378974cc2a90a0f
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-01 10:19:49 -0300

    chore: deleting unnecessary files.

---
+++ web/src/App.jsx
@@ -0,0 +1,24 @@
+const Title = ({title, children}) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+export const App = () => {
+  return (
+    <div id="tweet">
+      <div id="col-avatar" className="col">
+        <img src="/src/avatar.png" alt="" className="tweet__img" />
+      </div>
+      <div id="col-text" className="col">
+        <span>Elon Musk</span>
+        <span>@elonmusk</span>
+        <p>
+          Let´s make Twitter maximun fun
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
