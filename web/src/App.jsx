7a37f2abff1230dc20dd4e5f6654b575580f2696
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-02 14:28:47 -0300

    chore: create component TweetForm

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -11,9 +11,32 @@ const TweetForm = () => {
   }
 
   return (
+    <div className='border-b border-silver p-4 space-y-4'>
+      <div className="flex space-x-5">
+        <img src="/src/avatar.png" className='w-7' />
+        <h1 className='font-bold text-xl'>Página Inicial</h1>
+      </div>
+      <form className='pl-12 text-lg flex flex-col'>
+        <textarea 
+          name="text" 
           value={text}
+          placeholder="O que está acontecendo?" 
+          className='bg-transparent outline-none'
+          onChange={changeText} 
+        />
+        <div className='flex justify-end items-end space-x-3'>
+          <span className='text-sm'>
             <span>{text.length}</span> / <span className='text-birdBlue'>{MAX_TWEET_CHAR}</span>
+            </span>
+          <button 
+            className='bg-birdBlue py-1 px-6 rounded-full disabled:opacity-50'
             disabled={text.length > MAX_TWEET_CHAR ? true : false}
+          >
+            Tweet
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
