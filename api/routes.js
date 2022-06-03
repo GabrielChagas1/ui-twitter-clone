cb7225528cc6b1980baf14ea2467e14ddadfe433
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-03 10:57:07 -0300

    chore: creating get route to retrieve the tweets.

--- api/routes.js
+++ api/routes.js
@@ -4,3 +4,9 @@ export const router = new Router();
 
 const tweets = [];
 
+router.get('/tweets', ctx => {
+    ctx.body = ctx.query.username
+        ? tweets.filter(tweet => tweet.username === ctx.query.username) 
+        : tweets;
+});
+
