c472aa3a6d231ecd4e1d63d7760e0d415eee5c70
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-07 14:25:00 -0300

    chore: creating form login

--- web/src/Login/index.jsx
+++ web/src/Login/index.jsx
@@ -30,3 +30,49 @@ export function Login({signInUser}){
             password: ''
         }
     })
+    return (
+        <div className="flex h-full flex-col justify-center p-12 space-y-6">
+            <h1>Acesse sua conta</h1>
+
+            <form className="space-y-6" onSubmit={formik.handleSubmit}>
+                <div className='space-y-2'>
+                    <Input 
+                        name="email" 
+                        placeholder="E-mail"
+                        type="text"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        disabled={formik.isSubmitting}
+                    />
+                    {(formik.touched.email) && (formik.errors.email) && 
+                    (<span className='text-red-500 text-sm mt-2'>{formik.errors.email}</span>)}
+                </div>
+               <div>
+                <Input 
+                        name="password" 
+                        placeholder="Senha"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        disabled={formik.isSubmitting}
+                    />
+                {(formik.touched.password) && (formik.errors.password) && 
+                    (<span className='text-red-500 text-sm mt-2'>{formik.errors.password}</span>)}
+               </div>
+                <button 
+                    type='submit'
+                    className='bg-birdBlue py-4 rounded-full disabled:opacity-50 w-full text-lg'
+                    disabled={formik.isSubmitting || !formik.isValid}>
+                    {formik.isSubmitting ? 'Enviando...' : 'Entrar'}
+                </button>
+            </form>
+
+            <span className="text-sm text-silver text-center">
+                Não tem conta? <a className="text-birdBlue">Inscreva-se</a>
+            </span>
+
+        </div>
+    )
+}
