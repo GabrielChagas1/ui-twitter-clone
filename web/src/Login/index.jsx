ab7b46bed05d49fad555a14131646d988a8e9401
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-07 09:54:14 -0300

    chore: creating Input component.

--- web/src/Login/index.jsx
+++ web/src/Login/index.jsx
@@ -1,3 +1,8 @@
 import {useFormik} from 'formik'
 import * as yup from 'yup'
 import axios from 'axios'
+
+const Input = props => (
+    <input {...props} className="w-full bg-transparent p-4 border rounded border-onix text-lg outline-none focus:border-platinum"/>
+)
+
