b1103b389f643dcfca6d13b6fff1c858542e6314
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-07 09:55:07 -0300

    chore: creating validionSchema using yup.

--- web/src/Login/index.jsx
+++ web/src/Login/index.jsx
@@ -6,3 +6,8 @@ const Input = props => (
     <input {...props} className="w-full bg-transparent p-4 border rounded border-onix text-lg outline-none focus:border-platinum"/>
 )
 
+const validationSchema = yup.object({
+    email: yup.string().required('Digite seu email').email('E-mail inválido'),
+    password: yup.string().required('Digite sua senha')
+})
+
