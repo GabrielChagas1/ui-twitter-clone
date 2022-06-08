e70d58f2ff2024b6add096e03083340e1f9256f5
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-08 17:48:47 -0300

    chore: creating app

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,16 @@
 import {Home} from './Home'
 import {Login} from './Login'
+import { Signup } from './Signup'
 import { useState } from 'react'
 
 export default function App(){
     const [user, setUser] = useState()
 
-    return user ? <Home /> : <Login signInUser={setUser} />
+    if(user) return <Home loggedInUser={user} />
+
+    return window.location.pathname === '/signup'
+    ? <Signup signInUser={setUser} />
+    : <Login signInUser={setUser} />
+
+    // return user ? <Home /> : <Login signInUser={setUser} />
 }
